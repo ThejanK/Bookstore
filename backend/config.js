0e925bc2ee90de77fffd5ef2312f9c28eb861942
ThejanK <146336199+ThejanK@users.noreply.github.com>
2024-03-26 22:36:34 +0530

    first to commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@atlascluster.s2fcgj9.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasCluster';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
